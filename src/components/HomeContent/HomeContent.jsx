435eec6a3a7465397b91465094831dd700174c30
Yurii <131679817+havr1985@users.noreply.github.com>
2024-03-06 12:40:37 +0200

    fix static img

--- src/components/HomeContent/HomeContent.jsx
+++ src/components/HomeContent/HomeContent.jsx
@@ -1,10 +1,11 @@
 import { Title, Container, Pic } from "./HomeContent.styled"
+import carTitle from "../../assets/cartitle.png"
 
 export const HomeContent = () => {
     return (
         <Container>
             <Title>EASY Rent Cars</Title>
-            <Pic src="../../../public/car.png" alt="car" width={1000}/>
+            <Pic src={carTitle} alt="car" width={1000}/>
         </Container>
     )
 }
